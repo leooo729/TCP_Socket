40efd4803ddbc2cc2db3d9a3e018a2cc8d558e6b
Leo <leo8149736@gmail.com>
2022-10-07 09:54:56 +0800

    1007_0954

--- src/mysql/DatabaseCRUD.java
+++ src/mysql/DatabaseCRUD.java
@@ -2,81 +2,165 @@ package mysql;
 
 import org.json.JSONObject;
 
+import java.math.BigDecimal;
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class DatabaseCRUD {
-    public static String createMgni(JSONObject request){
-        Connection connection = DatebaseConnection.getConnection();
+    static Connection connection = DatebaseConnection.getConnection();
+
+    public static String createMgni(JSONObject request) {
         String result = "";
+        String id = "MGI" + DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS").format(LocalDateTime.now());
         try {
-            String createMgni = "insert into MGN_schema.CASHI values('MGI20221004100528100','"+request.getString("CASHI_ACC_NO")+"','"+request.getString("CASHI_CCY")+"',"+request.getBigDecimal("CASHI_AMT")+")";
             Statement st = connection.createStatement();
+            BigDecimal totalAmt = new BigDecimal(0);
+
+            String createCashi = "";
+            for (Object s : request.getJSONArray("accAmt")) {
+                JSONObject jsonObject = new JSONObject(s.toString());
+                System.out.println(jsonObject);
+                createCashi = "insert into MGN_schema.CASHI values('" + id + "','" + jsonObject.getString("acc") + "','" + request.getString("ccy") + "','" + jsonObject.getBigDecimal("amt") + "')";
+                totalAmt = totalAmt.add(jsonObject.getBigDecimal("amt"));
+                st.execute(createCashi);
+            }
+
+            String createMgni = "insert into MGN_schema.MGNI values('" + id
+                    + "','" + DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now())
+                    + "','1"
+                    + "','" + request.getString("cmNo")
+                    + "','" + request.getString("kacType")
+                    + "','" + request.getString("bankNo")
+                    + "','" + request.getString("ccy")
+                    + "','" + request.getString("pvType")
+                    + "','" + request.getString("bicaccNo")
+                    + "','" + request.getString("iType")
+                    + "','" + request.getString("pReason")
+                    + "'," + totalAmt
+                    + ",'" + request.getString("ctName")
+                    + "','" + request.getString("ctTel")
+                    + "','0"
+                    + "','" + DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now())
+                    + "')";
+
+            System.out.println(createMgni);
             st.execute(createMgni);
 
+        } catch (Exception x) {
+            System.out.println(x.toString());
         }
-        catch (Exception x) {
-            System.out.println("Exception :" + x.toString());
-        }
-        return result;
+        return "新增成功";
 
     }
+
     public static String getTargetMgni(JSONObject request) {
-        Connection connection = DatebaseConnection.getConnection();
         String result = "";
 
         try {
-            String getAllCashi = "SELECT * FROM MGN_schema.MGNI where MGNI_ID = '" + request.getString("id") + "'";
+            String targetMgni = "SELECT * FROM MGN_schema.MGNI where MGNI_ID = '" + request.getString("id") + "'";
             Statement st = connection.createStatement();
-            ResultSet resultSet = st.executeQuery(getAllCashi);
-            while (resultSet.next()) {
-                result = (resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getBigDecimal(4)+"\n");
-                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getBigDecimal(4));
+            ResultSet mgni = st.executeQuery(targetMgni);
+            while (mgni.next()) {
+                result = (" 申請ID:" + mgni.getString("MGNI_ID") + "\n"
+                        + " 存入日期:" + mgni.getString("MGNI_TIME") + "\n"
+                        + " 存入類型:" + mgni.getString("MGNI_TYPE") + "\n"
+                        + " 結算會員代號:" + mgni.getBigDecimal("MGNI_CM_NO") + "\n"
+                        + " 存入保管專戶別:" + mgni.getString("MGNI_KAC_TYPE") + "\n"
+                        + " 存入結算銀行代碼:" + mgni.getString("MGNI_BANK_NO") + "\n"
+                        + " 存入幣別:" + mgni.getString("MGNI_CCY") + "\n"
+                        + " 存入方式:" + mgni.getString("MGNI_PV_TYPE") + "\n"
+                        + " 實體帳號/虛擬帳號:" + mgni.getString("MGNI_BICACC_NO") + "\n"
+                        + " 存入帳號明細:"+"\n"+getTargetCashi(request)
+                        + " 存入類別:" + mgni.getString("MGNI_I_TYPE") + "\n"
+                        + " 存入實體帳號原因:" + mgni.getString("MGNI_P_REASON") + "\n"
+                        + " 總存入金額:" + mgni.getString("MGNI_AMT") + "\n"
+                        + " 聯絡人姓名:" + mgni.getString("MGNI_CT_NAME") + "\n"
+                        + " 聯絡人電話:" + mgni.getString("MGNI_CT_TEL") + "\n"
+                        + " 申請狀態:" + mgni.getString("MGNI_STATUS") + "\n"
+                        + " 更新時間:" + mgni.getString("MGNI_U_TIME") + "\n"
+                        + "\n");
             }
-        }
-//        catch (ClassNotFoundException e) {
-//            System.out.println("DriverClassNotFound :" + e.toString());
-//        }//有可能會產生sqlexception
-//        catch (SQLException x) {
-//            System.out.println("Exception :" + x.toString());
-//        }
-        catch (Exception x) {
-            System.out.println("Exception :" + x.toString());
+        } catch (Exception e) {
+            System.out.println("Exception :" + e.toString());
         }
         return result;
     }
+
     public static String getTargetCashi(JSONObject request) {
-        Connection connection = DatebaseConnection.getConnection();
-        String result="";
+        String result = "";
 
         try {
             String getAllCashi = "SELECT * FROM MGN_schema.Cashi where CASHI_MGNI_ID = '" + request.getString("id") + "'";
             Statement st = connection.createStatement();
             ResultSet resultSet = st.executeQuery(getAllCashi);
             while (resultSet.next()) {
-                result += ((resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getBigDecimal(4))+"\n");
-                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getBigDecimal(4));
+                result += ("    存入結算帳戶帳號:" + resultSet.getString("CASHI_ACC_NO") + "\n"
+                        + "    幣別:" + resultSet.getString("CASHI_CCY") + "\n"
+                        + "    金額:" + resultSet.getBigDecimal("CASHI_AMT") + "\n" + "\n");
             }
-        }
-//        catch (ClassNotFoundException e) {
-//            System.out.println("DriverClassNotFound :" + e.toString());
-//        }//有可能會產生sqlexception
-//        catch (SQLException x) {
-//            System.out.println("Exception :" + x.toString());
-//        }
-        catch (Exception x) {
-            System.out.println("Exception :" + x.toString());
+        } catch (Exception e) {
+            System.out.println("Exception :" + e.toString());
         }
         return result;
     }
 
-    public static String deleteMgni(JSONObject requset) {
-        Connection connection = DatebaseConnection.getConnection();
-        try {
+    public static String dynamicQueryMgni(JSONObject request) {
+        String result = "";
+        String cmNo = request.getString("cmNo");
+        String ccy = request.getString("ccy");
 
-            //要執行的命令
-            String deleteCashi = "delete from MGN_schema.CASHI where CASHI_MGNI_ID='"+requset.getString("id")+"'";
-            String deleteMgni = "delete from MGN_schema.MGNI where MGNI_ID='"+requset.getString("id")+"'";
+        String baseSQL = "SELECT * FROM MGN_schema.MGNI where 1=1";
+        StringBuilder builder = new StringBuilder();// 用於拼接SQL語句
+        builder.append(baseSQL);
+
+        if (!(cmNo).isEmpty()) {
+            builder.append(" and MGNI_CM_NO='" + cmNo + "'");
+        }
+        if (!(ccy).isEmpty()) {
+            builder.append(" and MGNI_CCY='" + ccy + "'");
+        }
+        try {
+            Statement st = connection.createStatement();
+            ResultSet mgni = st.executeQuery(builder.toString());
+            while (mgni.next()) {
+                result += (" 申請ID:" + mgni.getString("MGNI_ID") + "\n"
+                        + " 存入日期:" + mgni.getString("MGNI_TIME") + "\n"
+                        + " 存入類型:" + mgni.getString("MGNI_TYPE") + "\n"
+                        + " 結算會員代號:" + mgni.getBigDecimal("MGNI_CM_NO") + "\n"
+                        + " 存入保管專戶別:" + mgni.getString("MGNI_KAC_TYPE") + "\n"
+                        + " 存入結算銀行代碼:" + mgni.getString("MGNI_BANK_NO") + "\n"
+                        + " 存入幣別:" + mgni.getString("MGNI_CCY") + "\n"
+                        + " 存入方式:" + mgni.getString("MGNI_PV_TYPE") + "\n"
+                        + " 實體帳號/虛擬帳號:" + mgni.getString("MGNI_BICACC_NO") + "\n"
+                        + " 存入帳號明細:"+"\n"+getTargetCashi(request)
+                        + " 存入類別:" + mgni.getString("MGNI_I_TYPE") + "\n"
+                        + " 存入實體帳號原因:" + mgni.getString("MGNI_P_REASON") + "\n"
+                        + " 總存入金額:" + mgni.getString("MGNI_AMT") + "\n"
+                        + " 聯絡人姓名:" + mgni.getString("MGNI_CT_NAME") + "\n"
+                        + " 聯絡人電話:" + mgni.getString("MGNI_CT_TEL") + "\n"
+                        + " 申請狀態:" + mgni.getString("MGNI_STATUS") + "\n"
+                        + " 更新時間:" + mgni.getString("MGNI_U_TIME") + "\n"
+                        + "\n");
+            }//((resultSet.getString("MGNI_ID") + " " + resultSet.getString("MGNI_CM_NO") + " " + resultSet.getString("MGNI_CCY") + " " + resultSet.getBigDecimal("MGNI_CT_TEL")) + "\n");
+        } catch (Exception e) {
+            System.out.println("Exception :" + e.toString());
+        }
+        return result;
+    }
+
+    public static String updateMgni(JSONObject request) {
+        String updateMgni = "";
+
+        return "更新成功";
+    }
+
+    public static String deleteMgni(JSONObject requset) {
+        //要執行的命令
+        String deleteCashi = "delete from MGN_schema.CASHI where CASHI_MGNI_ID='" + requset.getString("id") + "'";
+        String deleteMgni = "delete from MGN_schema.MGNI where MGNI_ID='" + requset.getString("id") + "'";
+        try {
             //通過連接對象，將命令傳給資料庫
             Statement st = connection.createStatement();
             //執行
@@ -89,28 +173,6 @@ public class DatabaseCRUD {
     }
 
 
-//        Connection connection =DatebaseConnection.getConnection();
-//        try {
-
-//            要執行的命令
-//            String createCashi ="insert into MGN_schema.CASHI values('MGI20221004100528100','1','TWD',1000)";
-//            //通過連接對象，將命令傳給資料庫
-//            Statement st=connection.createStatement();
-//            //執行
-//            st.execute(createCashi);
-//            System.out.println("新增成功");
-
-//            String getAllCashi = "SELECT * FROM MGN_schema.CASHI";
-//            //通過連接對象，將命令傳給資料庫
-//            Statement st = connection.createStatement();
-//            //執行
-//            ResultSet resultSet = st.executeQuery(getAllCashi);
-//            while (resultSet.next()){
-//                System.out.println(resultSet.getString(1)+" "+resultSet.getString(2)+" "+resultSet.getString(3)+" "+resultSet.getBigDecimal(4));
-//            }
-//
-//
-//        }
 //        catch (ClassNotFoundException e) {
 //            System.out.println("DriverClassNotFound :" + e.toString());
 //        }//有可能會產生sqlexception
--- src/tcpSocket/SocketServer.java
+++ src/tcpSocket/SocketServer.java
@@ -6,23 +6,27 @@ import org.json.JSONObject;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
 
 public class SocketServer {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(1010); //服務氣端點對象
+        ServerSocket serverSocket = new ServerSocket(1010); //服務氣端點對象
+        while (true) {
+//            ServerSocket serverSocket = null;
+            try {
+//                serverSocket = new ServerSocket(1010); //服務氣端點對象
+                Socket socket = serverSocket.accept(); //監聽客服端
 
-            Socket socket = serverSocket.accept(); //監聽客服端
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream=socket.getOutputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(outputStream));
+                InputStream inputStream = socket.getInputStream();
+                OutputStream outputStream = socket.getOutputStream();
 
-            while (true) { // (line=br.readLine())!=null
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(outputStream));
 
-                JSONObject jsonObject = new JSONObject(br.readLine());
+                String requestStr = br.readLine();
+                if ("end".equals(requestStr)) {
+                    break;
+                }
+                JSONObject jsonObject = new JSONObject(requestStr);
                 String requestType = jsonObject.getString("requestType");
                 JSONObject request = jsonObject.getJSONObject("request");
                 //-------------------------------------------------------
@@ -35,50 +39,40 @@ public class SocketServer {
                         bw.write(DatabaseCRUD.getTargetMgni(request));
                         break;
                     }
-//                    case "2": {
-//                        bw.write(DatabaseCRUD.getTargetMgni(request));
-//                        break;
-//                    }
+                    case "2": {
+                        bw.write(DatabaseCRUD.dynamicQueryMgni(request));
+                        break;
+                    }
                     case "3": {
                         bw.write(DatabaseCRUD.deleteMgni(request));
                         break;
                     }
+                    case "4": {
+                        bw.write(DatabaseCRUD.createMgni(request));
+                        break;
+                    }
                     default: {
-
+bw.write("請輸入有效查詢資料");
                     }
                 }
                 bw.newLine();
                 bw.flush();
                 //-------------------------------------------------------
-                if ("bye".equals(requestType)) {
-                    break;
-                }
-//                request.close();
+                socket.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+//            finally {
+//                if (serverSocket != null) {
+//                    serverSocket.close();
+//                    System.out.println("接收結束");
+//                }
+//            }
 
-            br.close();
-            inputStream.close();
-            serverSocket.close();
-            System.out.println("接收結束");
-
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (serverSocket != null) {
-                serverSocket.close();
-            }
         }
-
     }
-    //-----------------------------------------------Method
-//private boolean sendData(OutputStream outputStream,JSONObject request){
-//    BufferedWriter request = new BufferedWriter(new OutputStreamWriter(outputStream);
-//    request.write(DatabaseCRUD.getTargetCashi(request.getString("id")));
-//    request.newLine();
-//    request.flush();
-//        return true;
-//}
 }
+
+
 
